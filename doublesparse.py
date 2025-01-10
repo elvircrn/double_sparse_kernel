936ca57836e515068f788d8922362c39f535d1c9
Elvir Crncevic <elvircrn@gmail.com>
2025-01-10 13:44:48 +0100

    actually work

--- doublesparse.py
+++ doublesparse.py
@@ -59,7 +59,6 @@ def find_other2(A, W, nnz, Z, U, reg=0, rho_start=0.03, rho=1, iters=5, prune_it
     return Z / norm2.unsqueeze(1), U / norm2.unsqueeze(1)
 
 
-
 def mag_prune(W, sp=0.6):
     thres = (W).abs().flatten().sort()[0][int(W.numel() * sp)]
     mask = ((W).abs() > thres)
@@ -71,6 +70,7 @@ def ent(p):
 
 
 def factorizeT(W, XX, asp=0.16, sp=0.4, iters=40, fixmask=None):
+    # W = lx.weight.detach().T.float()
     if fixmask is None:
         nza = int(W.shape[0] ** 2 * asp)
     else:
@@ -79,57 +79,79 @@ def factorizeT(W, XX, asp=0.16, sp=0.4, iters=40, fixmask=None):
 
     Az = torch.eye(W.shape[0], device=W.device)
     Au = torch.zeros_like(Az)
-    norm = XX.diagonal().sqrt_().unsqueeze(1) + 1e-8
+    norm = XX.diag().sqrt().unsqueeze(1) + 1e-8
+
     Wn = W * norm
+
     Bz = mag_prune(Wn, (1 - nzb / 2 / W.numel()))
     Bu = torch.zeros_like(Bz)
+
     for itt in range(iters):
+        # if itt < 10:
+        #    rho_start = 0.0
+        # elif itt < 15:
+        #    rho_start = 0.00
+        # else:
+        #    rho_start = 0.1
         rho_start = min(1.0, itt / (iters - 3)) ** 3
-        Az, Au = map(torch.t, find_other2(Bz.T, Wn.T, nza, Az.T, Au.T, reg=1e-2, rho_start=rho_start, fixmask=fixmask))
-        Bz, Bu = find_other2(Az, Wn, nzb, Bz, Bu, reg=1e-2, rho_start=rho_start)
+        Az, Au = (x.T for x in
+                  find_other2(Bz.T, Wn.T, nza, Az.T, Au.T, reg=1e-2, debug=False, rho_start=rho_start, fixmask=fixmask))
 
-    Az_nnz = (Az != 0).sum().item()
-    Bz_nnz = (Bz != 0).sum().item()
-    total_nnz = Az_nnz + Bz_nnz
-    ent_az = ent(Az_nnz / Az.numel())
-    ent_bz = ent(Bz_nnz / Bz.numel())
-    total_ent = (Az.numel() * ent_az + Bz.numel() * ent_bz) / W.numel()
+        Bz, Bu = find_other2(Az, Wn, nzb, Bz, Bu, reg=1e-2, debug=False, rho_start=rho_start)
 
-    print(total_nnz / W.numel(), Az_nnz / Az.numel(), Bz_nnz / Bz.numel(), Az.shape, Bz.shape, total_ent, ent(0.4), ent(0.5))
-    return (Az / norm) @ Bz, Bz.T, (Az / norm).T
+    print(((Az != 0).sum() + (Bz != 0).sum()).item() / W.numel(), (Az != 0).sum().item() / Az.numel(),
+          (Bz != 0).sum().item() / Bz.numel(), Az.shape, Bz.shape,
+          (Az.numel() * ent((Az != 0).sum().item() / Az.numel()) + Bz.numel() * ent(
+              (Bz != 0).sum().item() / Bz.numel())) / W.numel(),
+          ent(0.4), ent(0.5))
+    return ((Az / norm).matmul(Bz)).T, Bz.T, (Az / norm).T
 
 
 def factorizef(W, XX, asp=0.16, sp=0.4, iters=40, fixmask=None):
+    s_time = time.time()
     if W.shape[0] >= W.shape[1]:
-        return factorizeT(W.T, XX, asp, sp, fixmask)
+        return factorizeT(W.T, XX, asp, sp=sp, fixmask=fixmask)
 
-    nza = (fixmask != 0).sum().item() if fixmask is not None else int(W.shape[0] ** 2 * asp)
+    if fixmask is None:
+        nza = int(W.shape[0] ** 2 * asp)
+    else:
+        nza = (fixmask != 0).sum().item()
     nzb = int(W.numel() * sp - nza)
     if nzb < 0:
-        raise ValueError(f"Sparsity set too high: {sp}")
-    norm = XX.diagonal().sqrt_() + 1e-8
+        raise f'Error: sparsity set to high at {sp}'
+    norm = XX.diag().sqrt() + 1e-8
+
     Wn = W * norm
 
     Az = torch.eye(W.shape[0], device=W.device)
     Au = torch.zeros_like(Az)
-    Bz = mag_prune(Wn, 1 - nzb / (2 * W.numel()))
+
+    Bz = mag_prune(Wn, (1 - nzb / 2 / W.numel()))
     Bu = torch.zeros_like(Bz)
 
     for itt in range(iters):
+        # if itt < 10:
+        #    rho_start = 0.0
+        # elif itt < 15:
+        #    rho_start = 0.00
+        # else:
+        #    rho_start = 0.1
+
         rho_start = min(1.0, itt / (iters - 3)) ** 3
-        Az, Au = map(torch.t, find_other2(Bz.T, Wn.T, nza, Az.T, Au.T, reg=1e-2, rho_start=rho_start, fixmask=fixmask))
-        Bz, Bu = find_other2(Az, Wn, nzb, Bz, Bu, reg=1e-2, rho_start=rho_start)
+        Az, Au = (x.T for x in
+                  find_other2(Bz.T, Wn.T, nza, Az.T, Au.T, reg=1e-2, debug=False, rho_start=rho_start, fixmask=fixmask))
 
-    Az_nnz = (Az != 0).sum().item()
-    Bz_nnz = (Bz != 0).sum().item()
-    total_nnz = Az_nnz + Bz_nnz
-    ent_az = ent(Az_nnz / Az.numel())
-    ent_bz = ent(Bz_nnz / Bz.numel())
-    total_ent = (Az.numel() * ent_az + Bz.numel() * ent_bz) / W.numel()
+        Bz, Bu = find_other2(Az, Wn, nzb, Bz, Bu, reg=1e-2, debug=False, rho_start=rho_start)
 
-    print(total_nnz / W.numel(), Az_nnz / Az.numel(), Bz_nnz / Bz.numel(), Az.shape, Bz.shape, total_ent, ent(0.4), ent(0.5))
-    return Az @ (Bz / norm), Az, Bz / norm
+        # print(itt, time.time() - s_time, end =" ")
+        # print_scores(Az.matmul(Bz / norm))
 
+    print(((Az != 0).sum() + (Bz != 0).sum()).item() / W.numel(), (Az != 0).sum().item() / Az.numel(),
+          (Bz != 0).sum().item() / Bz.numel(), Az.shape, Bz.shape,
+          (Az.numel() * ent((Az != 0).sum().item() / Az.numel()) + Bz.numel() * ent(
+              (Bz != 0).sum().item() / Bz.numel())) / W.numel(),
+          ent(0.4), ent(0.5))
+    return Az.matmul(Bz / norm), Az, Bz / norm
 
 
 def finalize(XXb, W, Ab, Bb):
